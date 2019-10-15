325a33e6efdbf2c1e6db51f233919d186b03b488
Kennh <kennon_h@hotmail.com>
2019-10-14 19:11:45 -0700

    add input from file.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String s = in.nextLine();
-
-        if (s.equals("Caleb")) {
-            System.out.println("Hey Caleb");
-        } else if (s.equals("Sally")) {
-            System.out.println("Hey Sally");
-        } else {
-            System.out.println("Hey " + s);
-        }
-        in.close();
+//        System.out.println("Hello World!");
+//
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("What is your name?");
+//        String s = in.nextLine();
+//
+//        if (s.equals("Caleb")) {
+//            System.out.println("Hey Caleb");
+//        } else if (s.equals("Sally")) {
+//            System.out.println("Hey Sally");
+//        } else {
+//            System.out.println("Hey " + s);
+//        }
+       // in.close();
 
 //        int i = 0; //initialization
 //        while (i < 10) {  //condition
