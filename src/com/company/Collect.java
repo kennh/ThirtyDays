fc0e401657da80cfa18738ecab07ce71b7177bc9
Kennh <kennon_h@hotmail.com>
2019-10-14 19:35:20 -0700

    add input from file.

---
+++ src/com/company/Collect.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Collect {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner in = new Scanner(new File("text"));
+
+        List<String> students = new ArrayList<String>();
+        while(in.hasNextLine()) {
+            students.add(in.nextLine());
+        }
+
+        for(int i = 0; i < students.size(); i++) {
+            System.out.println("name: " + students.get(i));
+        }
+        in.close();
+    }
+}
+
